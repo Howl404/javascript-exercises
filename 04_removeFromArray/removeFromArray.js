26d867bf89f9c502d66a4bf05d7268ddfa7c9a26
Howl404 <114860794+Howl404@users.noreply.github.com>
2022-12-27 23:31:12 +0200

    First test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, remove) {
+  const index = arr.indexOf(remove);
+  arr.splice(index, 1);
+  return arr;
 };
 
+// [1, 2, 3, 4] - 3
 // Do not edit below this line
 module.exports = removeFromArray;
