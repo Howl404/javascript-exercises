a853174feac94368b1c3df45daa279a0e7a6ee25
Howl404 <114860794+Howl404@users.noreply.github.com>
2022-12-27 21:45:20 +0200

    4 tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (message, repeat) {
+  result = "";
+  for (let i = 0; i < repeat; i++) {
+    result += message;
+  }
+  return result;
 };
 
 // Do not edit below this line
