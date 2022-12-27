421441191d546199e52e63bbfd7e8b35bfd04803
Howl404 <114860794+Howl404@users.noreply.github.com>
2022-12-27 21:48:01 +0200

    All tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,13 @@
 const repeatString = function (message, repeat) {
+  if (repeat < 0) {
+    return "ERROR";
+  }
   result = "";
   for (let i = 0; i < repeat; i++) {
     result += message;
   }
   return result;
 };
-
+//
 // Do not edit below this line
 module.exports = repeatString;
