cfa843bfbf40f154cd57d848b70d5219e2f5967a
Howl404 <114860794+Howl404@users.noreply.github.com>
2022-12-27 22:56:30 +0200

    Pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let current = 0;
+  let result = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !(typeof num1 === "number") ||
+    !(typeof num2 === "number")
+  ) {
+    return "ERROR";
+  } else {
+    if (num1 < num2) {
+      for (let i = 1; i < num2 + 1; i++) {
+        current = i;
+        result += current;
+      }
+    } else {
+      for (let i = 1; i < num1 + 1; i++) {
+        current = i;
+        result += current;
+      }
+    }
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
