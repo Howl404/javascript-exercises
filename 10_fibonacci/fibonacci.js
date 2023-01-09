c5919eb8574dbb27590e3327c1e69e8530f67b7e
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-01-09 18:09:37 +0200

    +

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  a = a * 1;
+  if (a > 0) {
+    let numbers = [0, 1];
+    function fibonacciNumbers() {
+      for (let i = 1; i < 25; i++) {
+        numbers.push(numbers[i] + numbers[i - 1]);
+      }
+    }
+    fibonacciNumbers();
+    return numbers[a];
+  } else return "OOPS";
 };
 
 // Do not edit below this line
