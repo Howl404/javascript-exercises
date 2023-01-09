e41dd7d52ec3cdb2f991c9e53c01ae53a9746821
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-01-09 18:20:13 +0200

    +

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (object) {
+  let final = [];
+  for (let i = 0; i < object.length; i++) {
+    final.push(object[i].title);
+  }
+  return final;
 };
 
 // Do not edit below this line
